5f418922ea08abc6fe52d76d19e5f6cb43e3a6b4
Admin <madhu2012dxb@gmail.com>
2020-04-07 20:56:24 +0530

    LTTT

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,12 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
 	System.out.println("Test Git");
 	System.out.println("Test Git1");
 	    System.out.println("Test Git2");
 	    System.out.println("Test Git33");
+		System.out.println("Test Git77");
 	    System.out.println("Test Git66");
     }
 }
--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -5,5 +5,7 @@ public class Main2 {
     public static void main(String[] args) {
 	System.out.println("Test Git");
 	System.out.println("Test Git1");
+        System.out.println("Test Git4");
+        System.out.println("Test Git44");
     }
 }
